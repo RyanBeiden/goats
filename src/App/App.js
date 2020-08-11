df686687d26e5909714128647ba58142ec2ed822
Ryan Beiden <rdbeiden@gmail.com>
2020-08-10 22:06:44 -0400

    create first components

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
+
+import GoatCorral from '../components/GoatCorral/GoatCorral';
+import goatData from '../helpers/data/goatData';
+
 import './App.scss';
 
 class App extends React.Component {
+  state = {
+    goats: [],
+  }
+
+  componentDidMount() {
+    const goats = goatData.getGoats();
+    this.setState({ goats });
+  }
+
   render() {
+    const { goats } = this.state;
+
     return (
       <div className="App">
-        <h2>GOATS</h2>
-        <button className="btn btn-info"><i className="fas fa-bullseye"></i> I am a button</button>
+        <h1><span role="img" aria-label="goat emoji">🐐</span> Goats</h1>
+        <GoatCorral goats={goats}/>
       </div>
     );
   }
