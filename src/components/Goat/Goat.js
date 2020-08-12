534ac2a1fc235f4be74fe4c5dde0db080f2d16a2
Ryan Beiden <rdbeiden@gmail.com>
2020-08-11 19:58:11 -0500

    add functionality for freeing or taking a goat

--- src/components/Goat/Goat.js
+++ src/components/Goat/Goat.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import goatShape from '../../helpers/props/goatShape';
 
@@ -7,17 +8,33 @@ import './Goat.scss';
 class Goat extends React.Component {
   static propTypes = {
     goat: goatShape.goatShape,
+    changeGoatStatus: PropTypes.func,
+  }
+
+  goatStatusEvent = (e) => {
+    e.preventDefault();
+    const { goat, changeGoatStatus } = this.props;
+    changeGoatStatus(goat.id);
   }
 
   render() {
     const { goat } = this.props;
 
     return (
-      <div className="card goatCard">
+      <div className="card bg-dark goatCard">
         <img className="card-img-top" src={goat.imgUrl} alt="One specific goat" />
         <div className="card-body">
           <h5 className="card-title">{goat.name}</h5>
           <p className="card-text">This goat is {goat.age} {goat.age === 1 ? 'year' : 'years'} old and are described as: {goat.description}</p>
+        </div>
+        <div className="card-footer">
+          {
+            goat.isTaken ? (
+              <button className="btn btn-danger col-12" onClick={this.goatStatusEvent}>FREE THE GOAT</button>
+            ) : (
+              <button className="btn btn-success col-12" onClick={this.goatStatusEvent}>TAKE THE GOAT</button>
+            )
+          }
         </div>
       </div>
     );
